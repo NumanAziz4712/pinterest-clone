57365c403e0575c5fdf04ca2549d0d79ead1e8a0
Numan Aziz <numanaziz4712@gmail.com>
2022-03-22 19:56:16 +0500

    removed bug

--- components/Loader.js
+++ components/Loader.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Loader = () => {
   return (
     <div className='absolute z-[999] left-1/2 -translate-x-1/2 '>
-      <div class='h-10 w-10 p-3 animate-spin flex items-center justify-center rounded-full bg-black/70  mt-12 '>
-        <div class='grid grid-cols-2 place-items-center gap-y-1 gap-x-2'>
-          <span class='h-2 w-2 rounded-full bg-white inline-block'></span>
-          <span class='h-2 w-2 rounded-full bg-white inline-block'></span>
-          <span class='h-2 w-2 rounded-full bg-white inline-block'></span>
-          <span class='h-2 w-2 rounded-full bg-white inline-block'></span>
+      <div className='h-10 w-10 p-3 animate-spin flex items-center justify-center rounded-full bg-black/70  mt-12 '>
+        <div className='grid grid-cols-2 place-items-center gap-y-1 gap-x-2'>
+          <span className='h-2 w-2 rounded-full bg-white inline-block'></span>
+          <span className='h-2 w-2 rounded-full bg-white inline-block'></span>
+          <span className='h-2 w-2 rounded-full bg-white inline-block'></span>
+          <span className='h-2 w-2 rounded-full bg-white inline-block'></span>
         </div>
       </div>
     </div>
